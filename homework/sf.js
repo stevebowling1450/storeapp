36213e7ffe72b8d74dc246179a084bdf69cdeeea
Steve <stevebowling@SteveBowlingMBP.local>
2016-09-16 14:49:41 -0400

    3:00

--- homework/sf.js
+++
@@ -1,7 +0,0 @@
-var myCountry = "America";
-
-// Use console.log to print out the length of the variable myCountry.
-console.log(myCounty.length);
-
-// Use console.log to print out the first three letters of myCountry.
-console.log(myCountry.substring (0,3) );
