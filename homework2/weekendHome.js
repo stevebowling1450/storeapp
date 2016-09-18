a8ffcff2d401a906ee93b3158b6069e5c0682af7
Steve <stevebowling@SteveBowlingMBP.local>
2016-09-18 12:03:00 -0400

    Sun. noon

---
+++ homework2/weekendHome.js
@@ -0,0 +1,27 @@
+// Steve Bowling, TA Name: Josh, Date: 9-17-16
+
+// Problem 1 
+//var numArray = [18, 24, 57, 98, 45 ,27, 3, 456, 3, 35, 85];
+// var num= numArray.indexOf (27);
+// console.log(num);
+
+// Problem 2 
+
+// var x = numArray[0] + numArray[1]+numArray[2] +numArray[3] +numArray[4] +numArray[5] +numArray[6] +numArray[7] +numArray[8] +
+// numArray[9] +numArray[10] ;
+// console.log(x);
+
+// Problem 3
+// var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// a.pop();
+// console.log(a);
+
+// Problem 4 
+//  function count (p1, p2) {
+//            if (p1 + p2 <= 25)console.log("true")
+//             else  console.log("false")};
+//   count (10, 16);
+// Problem 5 
+
+
+// Problem 6
