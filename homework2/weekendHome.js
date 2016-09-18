adae506bd13c4159fd0619dd0212939a3ceccc5d
Steve <stevebowling@SteveBowlingMBP.local>
2016-09-18 13:00:36 -0400

    homework

--- homework2/weekendHome.js
+++ homework2/weekendHome.js
@@ -1,6 +1,7 @@
 // Steve Bowling, TA Name: Josh, Date: 9-17-16
 
 // Problem 1 
+
 //var numArray = [18, 24, 57, 98, 45 ,27, 3, 456, 3, 35, 85];
 // var num= numArray.indexOf (27);
 // console.log(num);
@@ -12,16 +13,40 @@
 // console.log(x);
 
 // Problem 3
+
 // var a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // a.pop();
 // console.log(a);
 
 // Problem 4 
+
 //  function count (p1, p2) {
 //            if (p1 + p2 <= 25)console.log("true")
 //             else  console.log("false")};
 //   count (10, 16);
 // Problem 5 
 
+// function string (s1, s2) {
+//         var x=(s1 +" "+ s2);
+//         var l=(x.length);
+//         if (l >=12)console.log("Error, string too long.");
+//     else console.log(x);}
+//    string ("hello","world" );
+
 
 // Problem 6
+
+// for (var i=0; i <=20; i ++)
+ 
+//  if (i <=5)
+// {console.log(i +" " + "Very Low Number")}
+
+// else if (i >=6 && i <=10 )
+// {console.log(i +" "+ "Low Number")}
+
+// else if (i >=11 && i <=15 )
+// {console.log(i+" "+ "High Number")}
+
+// else 
+// {console.log(i+
+//     " "+"Very High Number");}
