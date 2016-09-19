730b513f9d88618ecf2e9e6f5c3df02c5b92e593
Steve <stevebowling@SteveBowlingMBP.local>
2016-09-19 10:54:33 -0400

    hello

--- homework2/weekendHome.js
+++ homework2/weekendHome.js
@@ -2,15 +2,17 @@
 
 // Problem 1 
 
-//var numArray = [18, 24, 57, 98, 45 ,27, 3, 456, 3, 35, 85];
-// var num= numArray.indexOf (27);
-// console.log(num);
+// var numArray = [18, 24, 57, 98, 45 ,27, 3, 456, 3, 35, 85];
+//  var num= numArray.indexOf (27);
+//  console.log(num);
 
 // Problem 2 
-
-// var x = numArray[0] + numArray[1]+numArray[2] +numArray[3] +numArray[4] +numArray[5] +numArray[6] +numArray[7] +numArray[8] +
-// numArray[9] +numArray[10] ;
-// console.log(x);
+// var numArray = [18, 24, 57, 98, 45 ,27, 3, 456, 3, 35, 85];
+//  var x = numArray[0] + numArray[1]+numArray[2]
+// +numArray[3] +numArray[4] +numArray[5] +numArray[6]
+// +numArray[7] +numArray[8] +
+//   numArray[9] +numArray[10] ;
+//  console.log(x);
 
 // Problem 3
 
@@ -21,32 +23,34 @@
 // Problem 4 
 
 //  function count (p1, p2) {
-//            if (p1 + p2 <= 25)console.log("true")
-//             else  console.log("false")};
-//   count (10, 16);
+//                  if (p1 + p2 <= 25)console.log("true")
+//           else  console.log("false")};
+//              count (10, 14);
 // Problem 5 
 
-// function string (s1, s2) {
-//         var x=(s1 +" "+ s2);
-//         var l=(x.length);
-//         if (l >=12)console.log("Error, string too long.");
-//     else console.log(x);}
-//    string ("hello","world" );
+//  function string (s1, s2) {
+//               var x=(s1 +" "+ s2);
+//          var l=(x.length);
+//          if (l >=12)console.log("Error, string too long.");
+//      else console.log(x);}
+//     string ("hello","world" );
+
 
 
 // Problem 6
 
-// for (var i=0; i <=20; i ++)
+
+//   for (var i=0; i <=20; i ++)
  
-//  if (i <=5)
-// {console.log(i +" " + "Very Low Number")}
+//    if (i <=5)
+//   {console.log(i +" " + "Very Low Number")}
 
-// else if (i >=6 && i <=10 )
-// {console.log(i +" "+ "Low Number")}
+//   else if (i >=6 && i <=10 )
+//   {console.log(i +" "+ "Low Number")}
 
-// else if (i >=11 && i <=15 )
-// {console.log(i+" "+ "High Number")}
+//   else if (i >=11 && i <=15 )
+//   {console.log(i+" "+ "High Number")}
 
-// else 
-// {console.log(i+
-//     " "+"Very High Number");}
+//   else 
+//   {console.log(i+
+//       " "+"Very High Number");}
